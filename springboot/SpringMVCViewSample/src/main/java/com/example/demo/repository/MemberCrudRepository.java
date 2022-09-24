e97181c3ac9d51e99ea42e570cfaa7aa9c4d7ee2
subtoyomura <sub.toyomura@gmail.com>
2022-09-24 11:54:18 +0900

    springプロジェクト修正

--- springboot/SpringMVCViewSample/src/main/java/com/example/demo/repository/MemberCrudRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.repository;
-
-import org.springframework.data.repository.CrudRepository;
-
-import com.example.demo.entity.Member;
-
-public interface MemberCrudRepository extends CrudRepository<Member, Integer> {
-
-}
