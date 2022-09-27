6433b0f54f7ffca148144273032b15fdb483634a
subtoyomura <sub.toyomura@gmail.com>
2022-09-27 14:18:13 +0900

    課題2の途中

--- springboot/SpringMVCViewSample/src/main/java/com/example/demo/controller/TestController.java
+++ springboot/SpringMVCViewSample/src/main/java/com/example/demo/controller/TestController.java
@@ -1,15 +1,30 @@
 package com.example.demo.controller;
 
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+
+
+
 @Controller
 public class TestController {
 
-@GetMapping("test")
-	public String helloView(Model view) {
-		view.addAttribute("msg" , "Hello World");
+
+	@Autowired
+	private JdbcTemplate jdbcTemplate;
+
+	List<Map<String, Object>> list = jdbcTemplate.queryForList("select * from users");
+
+	@GetMapping("/test")
+	public String model(Model view) {
+		view.addAttribute("listTest",list);
+
 		return "test";
-	}
+}
 }
