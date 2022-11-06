fcda56b2cca69df0e34d88f3259f51d0ebd48833
subtoyomura <sub.toyomura@gmail.com>
2022-11-06 14:30:26 +0900

    11/6　課題3　途中

--- springboot/SpringMVCViewSample/src/main/java/com/example/demo/controller/BoardController.java
+++ springboot/SpringMVCViewSample/src/main/java/com/example/demo/controller/BoardController.java
@@ -4,9 +4,9 @@ import java.util.List;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -38,17 +38,15 @@ public class BoardController {
 	}
 
 	@GetMapping("/create")
-	public String create(@Validated
-            @ModelAttribute Board board,Model view) {
-		boardService.save(board);
+	public String create(Model view) {
 		return "create";
 	}
 
-	 @PostMapping("/store")
-	    public String create(Model model) {
-
-	        // login.htmlにリダイレクト
-	        return "redirect:/create";
+	 @PostMapping("/create/confirm")
+	    public String createConfirm(@Validated BoardForm boardForm, BindingResult result, Model view) {
+		 if(result.hasErrors()) {
+	        return "create";
 	    }
+	 return"create_confirm";
 	}
-
+}
---
+++ springboot/SpringMVCViewSample/src/main/java/com/example/demo/controller/BoardForm.java
@@ -0,0 +1,44 @@
+package com.example.demo.controller;
+
+
+	import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Size;
+
+
+	public class BoardForm {
+          @NotEmpty(message = "タイトルの入力は必須です")
+		  @Size(max = 20, message = "タイトルは20文字以内で入力してください")
+		private String title;
+
+          @NotEmpty(message = "つぶやきの入力は必須です")
+		  @Size(max = 140, message = "つぶやきは140文字以内で入力してください")
+		private String content;
+
+		public BoardForm() {
+
+		}
+		public BoardForm( String title, String content) {
+			super();
+			this.title = title;
+			this.content = content;
+
+		}
+
+		public String getTitle() {
+			return title;
+		}
+		public void setTitle(String title) {
+			this.title = title;
+		}
+		public String getContent() {
+			return content;
+		}
+		public void setContent(String content) {
+			this.content = content;
+		}
+
+
+	}
+
+
+
